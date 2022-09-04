ae76fdef9a02afc3870c9caecf7304038ce86eec
Vladislav izmailov <arielmap@icloud.com>
2022-09-04 15:52:17 +0300

    35

---
+++ 35/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] startArray = GetArray(123, 0, 1000);
+Console.WriteLine($"Колличество элментов в отрезке [10;99] = {GetCountElements(startArray, 10, 99)}");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int GetCountElements(int[] array, int leftRange, int rightRange)
+{
+    int count = 0;
+    foreach (var item in array)
+    {
+        if (item >= leftRange && item <= rightRange) ;
+    }
+    return count;
+}
